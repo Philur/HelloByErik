948e6b317b5b1df8156e652dc0885157fc373db4
Philur <philur@yahoo.com>
2014-11-07 10:57:12 +0100

    Add UnitTest2

---
+++ HelloByErik/HelloByErikTests/UnitTest2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HelloByErik;
+
+namespace HelloByErikTests
+{
+    [TestClass]
+    public class UnitTestA
+    {
+        [TestMethod]
+        public void TestMethodA()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+    }
+    [TestClass]
+    public class UnitTestB
+    {
+        [TestMethod]
+        public void TestMethodB()
+        {
+            Assert.AreEqual("Hello World", Program.CreateMessage());
+        }
+    }
+    [TestClass]
+    public class UnitTestC
+    {
+        [TestMethod]
+        public void TestMethodC()
+        {
+            Assert.AreEqual("Goodbye World", Program.CreateMessageGoodBye());
+        }
+    }
+    [TestClass]
+    public class UnitTestD
+    {
+        [TestMethod]
+        public void TestMethodD()
+        {
+            Assert.AreEqual("Goodbye World", Program.CreateMessageGoodBye());
+        }
+    }
+}
