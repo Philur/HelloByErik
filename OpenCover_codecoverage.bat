852d6263d7299307f65818045fab3bd152f65b49
Philur <philur@yahoo.com>
2014-11-11 15:58:42 +0100

    Deleted unneccessary test result files

--- OpenCover_codecoverage.bat
+++ OpenCover_codecoverage.bat
@@ -1,4 +1,3 @@
 "HelloByErik\packages\OpenCover.4.5.3207\OpenCover.Console.exe" -target:runtests.bat -register:user
 move results.xml Testresults\results.xml
 "HelloByErik\packages\ReportGenerator.2.0.1.0\ReportGenerator.exe" -reports:TestResults\results.xml -targetdir:TestResults\coverage
-pause
