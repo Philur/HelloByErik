ddd7d5f50abcebca1e8822c6f2fbb679f38062cc
Philur <philur@yahoo.com>
2014-11-13 07:57:28 +0100

    Removed MS CodeCoverage analysis

--- runtests.bat
+++ runtests.bat
@@ -1,1 +1,1 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" HelloByErik\HelloByErikTests\bin\Debug\HelloByErikTests.dll /logger:trx /EnableCodeCoverage /inIsolation
+"C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" HelloByErik\HelloByErikTests\bin\Debug\HelloByErikTests.dll /logger:trx /inIsolation
